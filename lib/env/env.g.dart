bf2a57be069dec19894ef9ace081565e54c54e21
Daoaoohwa <ldh05081@naver.com>
2025-06-05 16:29:09 +0900

    Update env.g.dart

--- lib/env/env.g.dart
+++ lib/env/env.g.dart
@@ -11,5 +11,5 @@ part of 'env.dart';
 // generated_from: .env
 final class _Env {
   static const String apiKey =
-      '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';
+      '';
 }
